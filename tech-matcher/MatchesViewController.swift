392074c20eb604e21319b01203f6222cfaea431f
Danilo Gomes <danilo_rs_gomes@hotmail.com>
2017-09-13 16:48:46 +0100

    fix for pictures

--- tech-matcher/MatchesViewController.swift
+++ tech-matcher/MatchesViewController.swift
@@ -16,6 +16,9 @@ class MatchesViewController: UITableViewController {
     var datasource : TMDatasource?
     var matches : [TMMatch] = []
     var selectedMatch : TMMatch?
+    var imageCache = NSCache<NSString, UIImage>()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +32,31 @@ class MatchesViewController: UITableViewController {
             let vc = segue.destination as! ChatViewController
             vc.loggedInUser = loggedInUser!
             vc.match = self.selectedMatch!
+        }
+    }
+    
+    func handlePicturesForCell(_ cell : MatchesTableViewCell,
+                               indexPath : IndexPath,
+                               data: TMMatch) {
+        
+        if let url = data.user.image {
+        
+            if let image = imageCache.object(forKey: url as NSString) {
+                cell.avatarImageView.image = image
+            } else {
+                
+                datasource?.downloadPicture(url: url, completionBlock: { (data, error) in
+                    if let data = data {
+                        if let image = UIImage(data: data) {
+                            self.imageCache.setObject(image, forKey: url as NSString)
+                            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                        }
+                    }
+                })
+                
+            }
+        } else {
+            cell.avatarImageView.image = #imageLiteral(resourceName: "match-placeholder")
         }
     }
 }
@@ -46,6 +74,7 @@ extension MatchesViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! MatchesTableViewCell
         let row = matches[indexPath.row]
         cell.populateWithMatch(row)
+        handlePicturesForCell(cell, indexPath: indexPath, data: row)
         return cell
     }
     
